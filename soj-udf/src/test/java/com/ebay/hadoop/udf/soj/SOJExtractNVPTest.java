da13a9e68d47f5e4bfefbadaa8163a4cad84449e
kajin <kajin@ebay.com>
2020-03-03 10:08:50 +0800

    fix issue on function SojExtractNVP and add new function DecodeSRPItmcardSig

--- soj-udf/src/test/java/com/ebay/hadoop/udf/soj/SOJExtractNVPTest.java
+++ soj-udf/src/test/java/com/ebay/hadoop/udf/soj/SOJExtractNVPTest.java
@@ -7,11 +7,12 @@ import static org.junit.Assert.assertEquals;
 
 public class SOJExtractNVPTest {
 
-	@Test
+    @Test
     public void testPositive() {
-		SojExtractNVP obj = new SojExtractNVP();
-        assertEquals("SojExtractNVP is working!", new Text("d9"), obj.evaluate("h8=d3&h4=d9&", "h4", "&" , "="));
+        SojExtractNVP obj = new SojExtractNVP();
+        assertEquals("SojExtractNVP is working!", new Text("d9"), obj.evaluate("h8=d3&h4=d9&", "h4", "&", "="));
+        assertEquals(new Text("7024841724"), obj.evaluate("bn:7024841724|", "bn", "|", ":"));
+
     }
-	
-	
+
 }
