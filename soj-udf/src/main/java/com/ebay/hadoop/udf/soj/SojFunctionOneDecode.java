9feea42f4f981142f6fdd8851998d349e6fd4830
kajin <kajin@ebay.com>
2020-03-17 11:43:27 +0800

    add tow functions SojFunctionNoDecode and SojFunctionOneDecode

---
+++ soj-udf/src/main/java/com/ebay/hadoop/udf/soj/SojFunctionOneDecode.java
@@ -0,0 +1,57 @@
+package com.ebay.hadoop.udf.soj;
+
+import java.util.LinkedHashMap;
+
+import com.ebay.hadoop.udf.utils.SojUtils;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentLengthException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.StringObjectInspector;
+
+public class SojFunctionOneDecode extends GenericUDF {
+    StringObjectInspector urlOI;
+    StringObjectInspector inputOI;
+    LinkedHashMap<Object, Object> result = new LinkedHashMap<Object, Object>();
+
+    @Override
+    public Object evaluate(DeferredObject[] args) throws HiveException {
+        String payload = urlOI.getPrimitiveJavaObject(args[0].get());
+        String keywords = inputOI.getPrimitiveJavaObject(args[1].get());
+        if (payload == null || keywords == null)
+            return null;
+
+        return SojUtils.parsePayload(payload, keywords, 1);
+    }
+
+    @Override
+    public String getDisplayString(String[] args) {
+        return "SojFuSojFunctionOneDecodenction to parse key-value pairs from behavior URL";
+    }
+
+    @Override
+    public ObjectInspector initialize(ObjectInspector[] args) throws UDFArgumentException {
+        if (args.length != 2) {
+            throw new UDFArgumentLengthException(
+                    "SojFunction takes two Strings as input, first is url, second is keys seperated by comma");
+        }
+        ObjectInspector a = args[0];
+        ObjectInspector b = args[1];
+
+        if (!(a instanceof StringObjectInspector) || !(b instanceof StringObjectInspector)) {
+            throw new UDFArgumentException("two arguments must be string");
+        }
+
+        this.urlOI = (StringObjectInspector) a;
+        this.inputOI = (StringObjectInspector) b;
+
+        return ObjectInspectorFactory.getStandardMapObjectInspector(
+                PrimitiveObjectInspectorFactory.javaStringObjectInspector,
+                PrimitiveObjectInspectorFactory.javaStringObjectInspector);
+    }
+
+    ;
+}
