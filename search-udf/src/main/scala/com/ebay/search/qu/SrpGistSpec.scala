2c83cba913aafd60eff63d543f3f65c74b3fe8f9
fwang12 <fwang12@ebay.com>
2021-11-04 22:42:10 +0800

    Remove unused scripts and add basic scala test (#101)

--- search-udf/src/main/scala/com/ebay/search/qu/SrpGistSpec.scala
+++ search-udf/src/main/scala/com/ebay/search/qu/SrpGistSpec.scala
@@ -12,15 +12,6 @@ class SrpGistSpec extends UDF {
   //   new BinaryMessageDecoder[Srp](new SpecificData(), Srp.SCHEMA$)
   lazy val srpDecoder = Decoders.srpGist
 
-  def test2() {
-    val encoded = "wwGnJNADX5smHgAAAgAAEDEyMzQ1Njc4EE9yaWdpbmFslKUE8JgEAAAKdJqY1qXEEADC/rvYtQ8AmNywvPwIALafsLz8CACum6fx1AYAtr/Ku/0PAIaG1fjvFgDOvOG8/Q8AkPXfrcQQAPqSq8D9DwCcktn83QoA5Ij4ockJAKj3vNu1DwD+vcXVjBEA7rey2Z0HAL6p5+TMDQDOwefkzA0ApJffpcQQAIqa8qjWEQDkofX7nRIAvLmqhZQKANjx7/qTCgDqxOPB8BIAgMmW28MQAMKIh4PqDgDEjNLOyAkA3o7ds54HAKrdzLaGDQDSpfzUtQ8Aosqjw/ASAIC504jqDgCGwp+XyQkA8sHqs4YNAN6Mj6vEEACMr9LM3goA0Ovw4ccUAJy6uPzvFgDGzJX+7xYA3PO94dQGAIKej/CCDQDUrKHJzQ0AiPjMgZ4SAK7aw9mTCgDQspirrwgAxsuR2ZAVALKw8sfeCgC41ILL3goAjMjv8J0SAKLPhLfdCgCYqYq9/Q8AvKbA6JMKALLk9/fvFgC21sekrwgAyN7Lsa8IALjnjojqDgCAx7GDnhIA7Mea7KILAOaF9KvEEAAAAA=="
-    val bytes = Base64.getDecoder.decode(encoded)
-    println("production")
-    println(bytesToHex(bytes))
-    val srp = srpDecoder.decode(bytes)
-    println("decoded srp from saas: " + srp)
-  }
-
   // def bytesToHex(bytes: Array[Byte]): String = bytes.map(String.format("%02X", java.lang.Byte.valueOf(_))).mkString(" ")
 
   def bytesToHex(bytes : Array[Byte]) = bytes.map{
