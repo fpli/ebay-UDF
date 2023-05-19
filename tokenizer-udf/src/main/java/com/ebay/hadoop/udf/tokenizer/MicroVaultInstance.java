1f28edd7f5207a3a50eb6dfc84f452fa5ba62a0d
fwang12 <fwang12@ebay.com>
2023-05-19 14:12:13 +0800

    [HADP-48836][FOLLOWUP] Wait the tokenizer service reachable for production environment (#161)

--- tokenizer-udf/src/main/java/com/ebay/hadoop/udf/tokenizer/MicroVaultInstance.java
+++ tokenizer-udf/src/main/java/com/ebay/hadoop/udf/tokenizer/MicroVaultInstance.java
@@ -3,17 +3,33 @@ package com.ebay.hadoop.udf.tokenizer;
 import com.ebay.platform.security.tokenizer.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MicroVaultInstance {
+  private static final Logger LOG = LoggerFactory.getLogger(MicroVaultInstance.class);
+
   public static String MICRO_VAULT_HOST_KEY = "MICRO_VAULT_HOST";
   public static String MICRO_VAULT_PORT_KEY = "MICRO_VAULT_PORT";
   public static String GRPC_BUFFER_SIZE_KEY = "GRPC_BUFFER_SIZE"; // in MB
   public static String DEFAULT_MICRO_VAULT_HOST = "localhost";
   public static int DEFAULT_MICRO_VAULT_PORT = 10000;
   public static int DEFAULT_GRPC_BUFFER_SIZE = 16 * 1024 * 1024; // 16MB
+
+  public static String POD_ENV_KEY = "POD_ENV";
+  public static String TOKENIZER_SERVICE_HOST_KEY = "TOKENIZER_SERVICE_HOST";
+  public static String TOKENIZER_SERVICE_PORT_KEY = "TOKENIZER_SERVICE_PORT";
+  public static String PROD_ENV = "production";
+  public static String DEFAULT_POD_ENV = PROD_ENV;
+  public static String DEFAULT_PROD_TOKENIZER_SERVICE_HOST = "tokenizersvc.vip.ebay.com";
+  public static int DEFAULT_PROD_TOKENIZER_SERVICE_PORT = 443;
 
   private final TokenizerGrpc.TokenizerBlockingStub tokenizerBlockingStub;
   private final MetadataGrpc.MetadataBlockingStub metadataBlockingStub;
@@ -29,6 +45,41 @@ public class MicroVaultInstance {
       }
     }
     return singleton;
+  }
+
+  private static boolean isHostPortReachable(String host, int port) {
+    try (Socket socket = new Socket()) {
+      InetSocketAddress socketAddress = new InetSocketAddress(host, port);
+      socket.connect(socketAddress, 1000); // connect timeout 1s
+      return true;
+    } catch (IOException e) {
+      LOG.warn("{}:{} is not reachable.", host, port);
+      return false;
+    }
+  }
+
+  /** Wait tokenizer service reachable for production environment. */
+  private static void waitProdTokenizerServiceReachable() {
+    String podEnv = Optional.ofNullable(System.getenv(POD_ENV_KEY)).orElse(DEFAULT_POD_ENV);
+    if (PROD_ENV.equalsIgnoreCase(podEnv)) {
+      String svcHost =
+          Optional.ofNullable(System.getenv(TOKENIZER_SERVICE_HOST_KEY))
+              .orElse(DEFAULT_PROD_TOKENIZER_SERVICE_HOST);
+      int svcPort =
+          Optional.ofNullable(System.getenv(TOKENIZER_SERVICE_PORT_KEY))
+              .map(i -> Integer.valueOf(i))
+              .orElse(DEFAULT_PROD_TOKENIZER_SERVICE_PORT);
+      while (!isHostPortReachable(svcHost, svcPort)) {
+        try {
+          LOG.info(
+              "Sleeping 1 second to wait the tokenizer service {}:{} reachable.", svcHost, svcPort);
+          TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+          // do nothing
+        }
+      }
+      LOG.info("Tokenizer service {}:{} is reachable.", svcHost, svcPort);
+    }
   }
 
   private MicroVaultInstance() {
@@ -53,6 +104,7 @@ public class MicroVaultInstance {
     ManagedChannel channel = channelBuilder.build();
     tokenizerBlockingStub = TokenizerGrpc.newBlockingStub(channel);
     metadataBlockingStub = MetadataGrpc.newBlockingStub(channel);
+    waitProdTokenizerServiceReachable();
   }
 
   /** message TokenizeResponse { string token = 1; string keyVersion = 2; bool keyEmbedded = 3; } */
