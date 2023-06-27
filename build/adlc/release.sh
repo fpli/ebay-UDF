0ea3858d7eb9b59bb1a9d931a7729b9a6ec92a0a
fwang12 <fwang12@ebay.com>
2023-06-27 21:22:55 +0800

    [ADLC] Add search-qu-udf back

--- build/adlc/release.sh
+++ build/adlc/release.sh
@@ -2,5 +2,4 @@
 
 WORKSPACE="$(cd "`dirname "$0"`/../.."; pwd)"
 
-# excluding search-qu-udf temporarily because of building issue
-mvn -s $WORKSPACE/build/adlc/maven-settings.xml clean package -f $WORKSPACE -DskipTests -U -pl bx-udf,common-udf,core-udf,dapgap-udf,datelib-udf,deploy,dss-shpmt-udf,ebaylib-udf,ecg-udf,ep-udf,hadoop-udf,nudata-udf,risk-udf,soj-udf,sys-udf,udf-tags,tokenizer-udf
+mvn -s $WORKSPACE/build/adlc/maven-settings.xml clean package -f $WORKSPACE -DskipTests -U
